75fdcfa5235ba52a6070fcdf42a94d34bb4641f5
Maria Arsuaga Rios <mariayh1@gmail.com>
2015-05-20 15:04:44 +0200

    Using Resource Annotation and PreConstruct and PostConstruct

--- spring-tutorial14/src/main/java/com/marsuaga/spring/annotations/Logger.java
+++ spring-tutorial14/src/main/java/com/marsuaga/spring/annotations/Logger.java
@@ -1,5 +1,7 @@
 package com.marsuaga.spring.annotations;
-
+import javax.annotation.PreDestroy;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -23,8 +25,7 @@ public class Logger {
 		this.consoleWriter = consoleWriter;
 	}
 	
-	@Autowired
-	@Qualifier("filewriter")
+	@Resource(name="squirrel")
 	public void setFileWriter(LogWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
@@ -35,6 +36,14 @@ public class Logger {
 	public void writeConsole(String text){
 		if(consoleWriter != null)
 			consoleWriter.write(text);
+	}
+	@PostConstruct
+	public void init(){
+		System.out.println("init");
+	}
+	@PreDestroy
+	public void destroy(){
+		System.out.println("destroy");
 	}
 	
 }
